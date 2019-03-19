3ac2842e0d96170125505d326ad4b052b7d3c983
Prateek Goyal <39560007+prateek2378@users.noreply.github.com>
2019-03-19 21:33:57 +0530

    Update apitweets.m

--- apitweets.m
+++ apitweets.m
@@ -4,19 +4,20 @@
 a=size(X,1);
 b=size(Y,1);
 n=zeros(a,2);
-j=0;p=1;
+j=1;p=1;
+n(1,1)=Y(1,2);
 for i=1:b;
   if p==1;
+    n(j,1)=Y(i,2);
+  end;
+  if Y(i,2)==n(j,1);
+    if Y(i,11)=='api';
+      c+=1;
+    end;
+  else
     n(j,2)=c;
     c=0;
-    p=0;
+    p=1;
     j+=1;
-    n(j,1)=Y(i,1);
   end;
-  if Y(i,11)=='api';
-    c+=1;
-  end;
-end;
-
-
 end;
