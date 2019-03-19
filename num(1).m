fd2cdef1a74685c69ccf3a6318e63dfb7d608a13
Prateek Goyal <39560007+prateek2378@users.noreply.github.com>
2019-03-19 20:50:27 +0530

    Update num(1).m

--- num(1).m
+++ num(1).m
@@ -5,15 +5,15 @@ a=size(X,1);
 b=size(X,2);
 n=zeros(a,1);
 for i=1:a;
-c=-1;
-for j=1:b;
-if X(i,j)==0;
-break
-end
-c+=1;
-end
+  c=-1;
+  for j=1:b;
+    if X(i,j)==0;
+    break
+    end
+  c+=1;
+  end
 n(i,1)=c;
 end;
 
-end;
- 
+
+end; 
