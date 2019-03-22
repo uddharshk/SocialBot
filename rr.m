bd7d654df92df529a2378734aacee83b71b57db5
laplacedemon7 <47855963+laplacedemon7@users.noreply.github.com>
2019-03-22 20:56:28 +0530

    Add files via upload

---
+++ rr.m
@@ -0,0 +1,24 @@
+function [Y]=rr(X) 
+Y=zeros(0,0);
+a=0;
+c=0;
+b=0;
+j=0;
+for i=1:size(X,1);
+if X(i,2)!=a && j!=0;
+Y=[Y;a,b,c];
+a=X(i,2);
+
+c=0;
+j=j+1;
+end
+b=0;
+if X(i,2)!=a && j==0;
+a=X(i,2);
+j+=1;
+end
+if X(i,14)==1;
+c+=1;
+end
+end
+end
